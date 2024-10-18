6e6bea0eea6a2a6d85c59af7d94c4307d5540ad2
hathsin <bwidner3@nmsu.edu>
2024-10-18 16:43:04 -0600

    removed unnecessary enclosing folder

---
+++ README.md
@@ -0,0 +1,3 @@
+# pagetrail
+
+A new Flutter project.
---
+++ lib/all_pages.dart
@@ -0,0 +1,5 @@
+//exporting all pages
+
+// Please export all your .dart pages here so that you can import them all at once in main.dart
+
+export 'homepage.dart';
