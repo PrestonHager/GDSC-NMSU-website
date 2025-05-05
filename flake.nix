66af47e1b8528cc7698f055d9efaf4adbd990e5f
Preston Hager <preston@hagerfamily.com>
2025-05-05 12:25:31 -0600

    Added chrome and brave support to flake

--- flake.nix
+++ flake.nix
@@ -8,29 +8,46 @@
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: {
     devShells = let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          flutter
-        ];
+      pkgs = import nixpkgs { inherit system; };
+      basePackages = with pkgs; [ flutter ];
+      nuPackages = with pkgs; [ flutter nushell ];
+      browserPackages = {
+        chrome = with pkgs; [ google-chrome ];
+        brave = with pkgs; [ brave ];
       };
-
-      nu = pkgs.mkShell {
-        packages = with pkgs; [
-          flutter
-          nushell
-        ];
-
-        env = {
-          CHROME_EXECUTABLE = "${pkgs.brave}/bin/brave";
-        };
-
-        shellHook = ''
-          exec nu
-        '';
+      makeShell = { name, packages, shellHook ? null, env ? null }: pkgs.mkShell {
+        inherit packages;
+        inherit shellHook;
+        inherit env;
+      };
+    in {
+      base = makeShell {
+        name = "base";
+        packages = basePackages;
+      };
+      "base-chrome" = makeShell {
+        name = "base-chrome";
+        packages = basePackages ++ browserPackages.chrome;
+        env.CHROME_EXECUTABLE = "${pkgs.chrome}/bin/google-chrome";
+      };
+      "base-brave" = makeShell {
+        name = "base-brave";
+        packages = basePackages ++ browserPackages.brave;
+        env.CHROME_EXECUTABLE = "${pkgs.brave}/bin/brave";
+      };
+      nu = makeShell { name = "nu";
+        packages = nuPackages;
+        shellHook = ''exec nu'';
+      };
+      "nu-chrome" = makeShell { name = "nu-chrome";
+        packages = nuPackages ++ browserPackages.chrome;
+        shellHook = ''exec nu'';
+        env.CHROME_EXECUTABLE = "${pkgs.chrome}/bin/google-chrome";
+      };
+      "nu-brave" = makeShell { name = "nu-brave";
+        packages = nuPackages ++ browserPackages.brave;
+        shellHook = ''exec nu'';
+        env.CHROME_EXECUTABLE = "${pkgs.brave}/bin/brave";
       };
     };
   });
